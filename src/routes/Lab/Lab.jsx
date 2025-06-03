e2a5d594bac54cf59949bd9f0bfae7d06b5ea3ff
textencoder <160430049+textencoder@users.noreply.github.com>
2025-06-03 12:44:19 -0500

    - added functionality for randomize button - decreased size of h1 elements on home splash - changed default zoom to 55 and set minimum value to 10

--- src/routes/Lab/Lab.jsx
+++ src/routes/Lab/Lab.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { parse, stringify } from "svgson";
-import "../../styles/App.css"
+import "../../styles/App.css";
 import styles from "./Lab.module.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
@@ -10,17 +10,32 @@ export default function Lab() {
   const [name, setName] = useState("");
   const [vector, setVector] = useState(null);
 
-  const [primaryColor, setPrimaryColor] = useState({ r: 255, g: 92, b: 0, a: 1 });
-  const [secondaryColor, setSecondaryColor] = useState({ r: 255, g: 255, b: 255, a: 1 })
-  const [backgroundColor, setBackgroundColor] = useState({ r: 147, g: 161, b: 176, a: 0.05 })
+  const [primaryColor, setPrimaryColor] = useState({
+    r: 255,
+    g: 92,
+    b: 0,
+    a: 1,
+  });
+  const [secondaryColor, setSecondaryColor] = useState({
+    r: 255,
+    g: 255,
+    b: 255,
+    a: 1,
+  });
+  const [backgroundColor, setBackgroundColor] = useState({
+    r: 147,
+    g: 161,
+    b: 176,
+    a: 0.05,
+  });
 
-  const [zoom, setZoom] = useState(100)
+  const [zoom, setZoom] = useState(55);
 
   const randomNumber = () => {
-    return Math.floor(Math.random() * 24)
-  }
+    return Math.floor(Math.random() * 24);
+  };
 
-    const [count, setCount] = useState(() => randomNumber());
+  const [count, setCount] = useState(() => randomNumber());
   useEffect(() => {
     fetch("/vectors.json")
       .then((response) => {
@@ -36,30 +51,30 @@ export default function Lab() {
   return (
     <div className={styles.labBackground}>
       <div className={styles.labWrapper}>
-      <Header 
-      name={name}
-      vector={vector}
-      count={count}
-      setCount={setCount}
-      setVector={setVector}
-      />
+        <Header
+          name={name}
+          vector={vector}
+          count={count}
+          setCount={setCount}
+          setVector={setVector}
+        />
 
-      <Viewport 
-      zoom={zoom}
-      vector={vector}
-      primaryColor={primaryColor}
-      backgroundColor={backgroundColor}
-      />
-      
-      <Footer
-      zoom={zoom}
-      setZoom={setZoom}
-      primaryColor={primaryColor}
-      setPrimaryColor={setPrimaryColor}
-      backgroundColor={backgroundColor}
-      setBackgroundColor={setBackgroundColor}
-      />
-    </div>
+        <Viewport
+          zoom={zoom}
+          vector={vector}
+          primaryColor={primaryColor}
+          backgroundColor={backgroundColor}
+        />
+
+        <Footer
+          zoom={zoom}
+          setZoom={setZoom}
+          primaryColor={primaryColor}
+          setPrimaryColor={setPrimaryColor}
+          backgroundColor={backgroundColor}
+          setBackgroundColor={setBackgroundColor}
+        />
+      </div>
     </div>
   );
 }
