99909a2937075e2435fd79d7b6dc85e9c2c47a8c
textencoder <160430049+textencoder@users.noreply.github.com>
2025-06-03 23:04:10 -0500

    reduced splash button margin and changed randomize values to be 0-255 inclusive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -217,15 +217,15 @@ function RandomizeButton(props) {
   
   function handleRandomize() {
     props.setPrimaryColor({
-      r: Math.floor(Math.random() * 255),
-      g: Math.floor(Math.random() * 255),
-      b: Math.floor(Math.random() * 255),
+      r: Math.floor(Math.random() * 256),
+      g: Math.floor(Math.random() * 256),
+      b: Math.floor(Math.random() * 256),
       a: 1
     })
     props.setBackgroundColor({
-      r: Math.floor(Math.random() * 255),
-      g: Math.floor(Math.random() * 255),
-      b: Math.floor(Math.random() * 255),
+      r: Math.floor(Math.random() * 256),
+      g: Math.floor(Math.random() * 256),
+      b: Math.floor(Math.random() * 256),
       a: 1
     })
   }
