72d1980d2cb3cc913c323efe3e0145c864b42e49
textencoder <160430049+textencoder@users.noreply.github.com>
2025-06-04 16:21:01 -0500

    - set lab footer max-height - restyled lab header     - removed share button     - added home button     - moved dropdown to right side - replaced a tags with Link tags

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,5 +1,6 @@
 import styles from "./Home.module.css";
 import Typewriter from "typewriter-effect";
+import { Link } from "react-router-dom"
 
 export default function Home() {
   const nouns = ["Redefined", "Reimagined", "Rewritten", "Recharged"];
@@ -10,7 +11,7 @@ export default function Home() {
 
       <div className={styles.splash}>
         <Heading />
-        <a href="/lab">
+        <Link to="/lab">
           <button className="aktiv-regular">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +24,7 @@ export default function Home() {
             </svg>
             <p>Enter the Lab</p>
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
@@ -36,6 +37,7 @@ function Nav() {
         <div className={styles.logo}></div>
 
         <ul className="aktiv-regular">
+          <Link to="/lab">
           <li>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -46,8 +48,10 @@ function Nav() {
             >
               <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
             </svg>
-           <a href="/lab">About</a>
+           <p>About</p>
           </li>
+          </Link>
+          <Link to="/lab">
           <li>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -58,8 +62,10 @@ function Nav() {
             >
               <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
             </svg>
-            <a href="/lab">Shop</a>
+            <p>Shop</p>
           </li>
+          </Link>
+          <Link to="/lab">
           <li>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -70,8 +76,9 @@ function Nav() {
             >
               <path d="M200-120q-51 0-72.5-45.5T138-250l222-270v-240h-40q-17 0-28.5-11.5T280-800q0-17 11.5-28.5T320-840h320q17 0 28.5 11.5T680-800q0 17-11.5 28.5T640-760h-40v240l222 270q32 39 10.5 84.5T760-120H200Zm80-120h400L544-400H416L280-240Zm-80 40h560L520-492v-268h-80v268L200-200Zm280-280Z" />
             </svg>
-            <a href="/lab">Lab</a>
+           <p>Lab</p>
           </li>
+           </Link>
         </ul>
       </div>
     </nav>
