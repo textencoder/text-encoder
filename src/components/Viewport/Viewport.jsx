8bd3c47f2c7f6cb17ae74b8fde66b5d625e936a9
textencoder <160430049+textencoder@users.noreply.github.com>
2025-07-04 11:39:54 -0500

    - installed react-aria - increased viewport size to 100% - changed header and control center positioning to fixed - removed unnecessary code

--- src/components/Viewport/Viewport.jsx
+++ src/components/Viewport/Viewport.jsx
@@ -6,7 +6,6 @@ export default function Viewport(props) {
     return (
         <div className={styles.viewportOuter}>
         <ReactSVG
-                className={styles.viewportInner}
                 style={{backgroundColor: `rgba(${props.backgroundColor.r}, ${props.backgroundColor.g}, ${props.backgroundColor.b}, ${props.backgroundColor.a})`}}
                  afterInjection={(svg) => {
                    svg.setAttribute('style', `fill: rgba(${props.primaryColor.r}, ${props.primaryColor.g}, ${props.primaryColor.b}, ${props.primaryColor.a}); transform: scale(${props.zoom / 100})`);
